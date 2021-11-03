ae517b6e3420b08d976abf6eadad834fab191e90
jorchsg <sandoval.jorge.407@gmail.com>
2021-11-02 18:35:44 -0700

    renaming files and add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+//Importing Pages
+import Home from './pages/Home.js';
+import Register from './pages/Register.js';
+import Login from './pages/Login.js';
+import MyMeals from './pages/MyMeals.js';
+import Dashboard from './pages/Dashboard.js';
+import Profile from './pages/Profile.js';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello From App</h1>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/login" exact component={Login}></Route>
+          <Route path="/register" exact component={Register}></Route>
+          <Route path="/dashboard" exact component={Dashboard}></Route>
+          <Route path="/my-meals" exact component={MyMeals}></Route>
+          <Route path="/profile" exact component={Profile}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
