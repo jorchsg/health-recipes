85cba5a479b51056143520d8cbddc623f921c82f
jorchsg <sandoval.jorge.407@gmail.com>
2021-11-03 01:25:14 -0700

    fix files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import Header from '../components/Header';
+import FoodHome from '../assets/images/food_home.png';
 
 const Home = () => {
     return (
         <>
-            <h1>Landing Page</h1>
+            <Header></Header>
+            <div className="home_page_container">
+                <div className="home_page_content">
+                    <div className="home_page_content_left">
+                        <h1>Recepies that will comein handy for your diet</h1>
+                        <button>Lets Get Started</button>
+                    </div>
+                    <div className="home_page_content_right">
+                        <img src={FoodHome} alt="food home" />
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
 
-export default Home
+export default Home;
