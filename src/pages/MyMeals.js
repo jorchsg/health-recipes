de416d7aa872c61550a27b78809297af775cc3af
jorchsg <sandoval.jorge.407@gmail.com>
2021-11-28 21:31:25 -0800

    initialize my meals page

--- src/pages/MyMeals.js
+++ src/pages/MyMeals.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import HeaderAuth from '../components/HeaderAuth';
+import './myMeals.scss'
 
 const MyMeals = () => {
     return (
         <>
             <HeaderAuth />
-            <h1>My Meals Page</h1>
+            <div className="my__meals__content container">
+                <h1>My Meals Page</h1>
+            </div>
         </>
     )
 }
